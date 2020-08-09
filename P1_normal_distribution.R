213a9a8ca577b8bc34e41c25ae96f6bf7d2c5551
ShKlinkenberg <s.klinkenberg@gmail.com>
2020-08-09 12:56:23 +0200

    working on borders and margins

--- P1_normal_distribution.R
+++ P1_normal_distribution.R
@@ -2,10 +2,15 @@ x = seq(-4,4,.01)
 
 y = dt(x, 4)
 
+par(mar  = c(1,1,1,1),
+    omd  = c(1,1,1,1),)
+
 plot(x,y,
      xlab = "",
+     xaxt = "n",
      ylab = "",
-     bty  + "n"
+     yaxt = "n",
+     bty  = "n"
      )
 
 polygon(c(x,rev(x)), c(y, y*0), 
